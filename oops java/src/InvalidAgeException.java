9fe31a407a092a4b43196e9e56be690728a65fb7
Java <Amritanshuchaudhary12345@gmail.com>
2025-08-28 09:46:30 +0530

    Exception_28/08/25

---
+++ oops java/src/InvalidAgeException.java
@@ -0,0 +1,8 @@
+//step 1
+public class InvalidAgeException extends Exception{
+//step 2
+    public InvalidAgeException(String messege){
+        super(messege);
+
+    }
+}
---
+++ oops java/src/UserDefinedException.java
@@ -0,0 +1,8 @@
+public class UserDefinedException {
+    public void validate(int age)throws InvalidAgeException{
+        if(age<18)
+            throw new InvalidAgeException("Not eligible to Vote age less than 18");
+        else
+            System.out.println("Eligible to Vote");
+    }
+}
